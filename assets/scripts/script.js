d63ecfd81922947bf2e9005a3d5167de7897b720
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-05-02 00:54:13 -0700

    added display for timeblocks with css preset active

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,76 @@
-//Var Declarations
-const displayMsg = {
-  // TODO: if needed 
+/*Removed from HTML
+
+      <!-- Use class for "past", "present", and "future" to apply styles to the
+        time-block divs accordingly. The javascript will need to do this by
+        adding/removing these classes on each div by comparing the hour in the
+        id to the current hour. The html provided below is meant to be an example
+        demonstrating how the css provided can be leveraged to create the
+        desired layout and colors. The html below should be removed or updated
+        in the finished product. Remember to delete this comment once the
+        code is implemented.
+        -->
+
+      <!-- Example of a past time block. The "past" class adds a gray background color. -->
+      <div id="hour-9" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>
+
+      <!-- Same as above except The "present" class adds a red background color. -->
+      <!-- Same as above except The "future" class adds a green background color. -->
+*/
+
+//Debug Printer
+function p(displayMe){console.log(displayMe);}
+
+//#region Constants Contingents
+const workDay = {startTime: 9, duration: 8}; // change to appropriate variable shift
+// #endregion
+
+//#region Draw Time Blocks
+const containerID = document.getElementById("container"); p(containerID);
+
+function createChildOf(parentObj, childString){
+  let child = document.createElement(childString);
+  parentObj.appendChild(child);
+  return child;
 }
 
+function createTimeBlock(hourNum){
+  let timeBlock = createChildOf(containerID, "div");
+  timeBlock.id = "hour-"+hourNum;
+  timeBlock.className = "row time-block future";
+  
+  hour = createChildOf(timeBlock,"div");
+  hour.className = "col-2 col-md-1 hour text-center py-3";
+  hour.textContent = dayjs().set('hour',hourNum).set('minute',0).set('seconds',0).format("hA");
 
+  description = createChildOf(timeBlock,"textarea");
+  description.className = "col-8 col-md-10 description";
+  description.setAttribute("rows", 3);
 
+  button = createChildOf(timeBlock,"button");
+  button.className = "btn saveBtn col-2 col-md-1";
+  button.setAttribute("aria-label","save");
+  
+  iTag = createChildOf(button,"i");
+  iTag.className = "fas fa-save";
+  iTag.setAttribute("aria-hidden","true");
+}//forgning presets
+
+for(i=0; i<=workDay.duration;i++){
+  createTimeBlock(workDay.startTime+i); //p(`Drawing ${i+1}th t-block`);
+}
+//#endregion
 
 
 /* Wrap all code that interacts with the DOM in a call to jQuery to ensure that
  * the code isn't run until the browser has finished rendering all the elements
  * in the html.
 */
-
-
 
 $(document).ready(function() {
   /*// TODO: Add a listener for click events on the save button
@@ -25,26 +83,50 @@ $(document).ready(function() {
    * useful when saving the description in local storage?
   */
 
-  //obj.addListener('click',/*fnName*/);
+
+  // get a specific hour's button
+  // add a listener to that
+  // save that hour's value to local storage
+/*CHatGPT
+  var saveBtns = document.querySelectorAll(".saveBtn");
+  
+  for (var i = 0; i < saveBtns.length; i++) {
+    saveBtns[i].addEventListener("click", function(event) {
+      // get the value of the textarea for this button's row
+      p('IN HERE!!');
+      p(i);
+      var row = event.target.parentNode;
+      var textarea = row.querySelector(".description");
+      var text = textarea.value.trim();
+      var storage ="text"+i;
+      p(storage);
+      localStorage.setItem(storage, text);
+    });
+  }
+*/
+
+  function loadData(){
+    localStorage.getItem();
+  }
+
+  function saveData(){
+    localStorage.setItem();
+  }
 
   /*// TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour.
    * HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? 
    * How can Day.js be used to get the current hour in 24-hour time?
   */
   
-  
 
-  var hour9 = dayjs().set('hour',9).set('minute',0).set('seconds',0);
-
-  console.log(hour9);
-
+  /*
   console.log(dayjs().isBefore(hour9));
   
   if (dayjs().isAfter(hour9)){
     //make hour nine 9
     //.addClass FROM jquery
   }
-
+  */
 
   /*compareID(){
     // get timeNow
